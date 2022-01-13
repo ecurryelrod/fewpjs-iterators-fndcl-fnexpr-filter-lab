248958cc6632057a4e717e71f6be9d5066f48cea
Erika Curry-Elrod <ecurryelrod@gmail.com>
2022-01-12 18:42:28 -0700

    commit tree 72d2fbcbf4df90f6146cab86a574d000f9eed52d

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+    return array.filter(possibleMatch => possibleMatch.toLowerCase() === string.toLowerCase())
+}
+
+function fuzzyMatch(array, letters) {
+    return array.filter(word => word.toLowerCase().indexOf(letters.toLowerCase()) === 0)
+}
+
+function matchName(array, string) {
+    return array.filter(record => record.name === string)
+}
